c6016ba2ded26ba5586b3f162fb0808be76ba05e
tonakihan <116086641+tonakihan@users.noreply.github.com>
2023-09-11 18:40:08 +0900

    Add ejs

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-import express from "express";
-import path from 'path';
-import {spawn} from "node:child_process";
+import express from 'express'
+import engine from 'ejs-mate'
+import path from 'path'
+// import {spawn} from "node:child_process";
 
 const PORT = process.env.PORT || 3000;
-const app = express();
 const __dirname = path.resolve();
 
+const app = express();
+
+app.use(express.static(__dirname + '/public'))
+app.engine('ejs', engine) //ejs-mate
+app.set('views', __dirname + '/views'); //Папка для render
+app.set('view engine', 'ejs'); //use .ejs файлы
+
+
 app.listen(PORT, () => {
-    console.log(`Server starting on port ${PORT}`);
+    console.log(
+        `Server starting on port ${PORT}\n` +
+        `  http://localhost:${PORT}`
+    );
 });
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'views')); //Папка для render
-app.set('view engine', 'ejs'); //+.ejs для файлов (автоматика)
-
+app.get('/', (req, res) => {
+  res.render('page/index', {"title": "homepage"})
+})
 
 app.get("/api", (req, res) => {
     res.json({
