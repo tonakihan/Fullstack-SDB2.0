b9a5ca516a7e9427420d115a08125ea56f89e7cc
tonakihan <116086641+tonakihan@users.noreply.github.com>
2024-01-03 11:49:20 +0900

    I do not remember

--- public/scripts/dynamic-select.js
+++ public/scripts/dynamic-select.js
@@ -1,13 +1,45 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let form = document.forms.one;
-    let select = form.elements.target;
-    let div = document.getElementById("output");
+    let eForm = document.forms.one;
+    let eSelect = eForm.elements.target;
+    let eDiv = document.getElementById("output");
+    
+    let url = "/api";
 
-    let usrTarget = select.value;
-    div.innerHTML = usrTarget; // Надо придумать
-
-    select.onchange = () => {
-        usrTarget = select.value;
-        div.innerHTML = usrTarget; // Ща посмотрим
+    eSelect.onchange = () => {
+        eDiv.innerHTML = ""; 
     }
+
+    eForm.addEventListener("submit", (evt) => {
+        evt.preventDefault(); //Отмена события - по умолчанию
+        
+        if (eSelect.value == "Выбери таблицу" || eSelect.value == "") {
+            alert("Выбери цель из списка для запроса");
+        }
+        else {
+            //TODO: Добавить кружочек загрузки
+            eDiv.innerHTML = "Loading..."
+            try {
+                getDataServer(url + "?target=" + eSelect.value)
+                    .then(data => {
+                        console.log(data);
+                        eDiv.innerHTML = data;
+                });
+            } catch (e) {
+                alert("Провал запроса. Ошибка.");
+                console.error(e);
+        }}
+    })
+});
+
+
+let getDataServer = new Promise((res, rej) => {
+    const response = await fetch(reqUrl, {
+        method: "GET",
+    })
+    if (response.ok === false) {
+        alert("Провал запроса. Ошибка.")
+    }
+
+    let data = await response.json().data;
+    resolve(data);
 });
